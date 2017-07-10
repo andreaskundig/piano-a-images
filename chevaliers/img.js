0204a5ddb9b3af53befae8812649eee24d854175
andreas kundig <andreas.kuendig@gmx.ch>
2017-07-10 22:19:31 +0200

    sounds

--- chevaliers/img.js
+++ chevaliers/img.js
@@ -1,1 +1,88 @@
-var imageSequence = ['01.png','02.png','03.png','04.png','05.png','06.png','07.png','08.png','09.png','10.png','11.png','12.png'];
+// 13
+// 20
+// 17
+// 12
+
+// 15
+// 22
+// 21
+// 16
+
+// 19
+// 14
+// 11
+// 18
+
+var composition ={ imageSequence : [
+    null, //1 
+    null, //2 
+    null, //3 
+    null, //4 
+    null, //5 
+    null, //6 
+    null, //7 
+    null, //8 
+    null, //9 
+    null, //10 
+    '11.png', //11 presque poignee
+    '04.png', //12 devant chateau noir
+    '01.png', //13 paysage
+    '10.png', //14 tend la main
+    '05.png', //15 devant chateau blanc
+    '08.png', //16 souleve le casque
+    '03.png', //17 dessus chateau blanc
+    '12.png', //18 chute
+    '09.png', //19 enleve le casque
+    '02.png', //20 dessus chateau noir 
+    '07.png', //21 baisse la garde
+    '06.png', //22 face a face
+]},
+//13 paysage
+//20 dessus chateau noir 
+//17 dessus chateau blanc
+//12 devant chateau noir
+//15 devant chateau blanc
+//22 face a face
+//21 baisse la garde
+//16 souleve le casque
+//19 enleve le casque
+//14 tend la main
+//11 presque poignee
+//18 chute
+    makePartition = function(){
+        var notes = [13,20,17,12,15,22,21,16,19,14,11,18],
+            duration = 2.5 ,
+            partition = [],
+            nextT = 0;
+        
+        notes.forEach(function(n){
+            partition.push({m:144, n:35 + n, t:nextT});
+            nextT += duration;
+            partition.push({m:128, n:35 + n, t:nextT});
+        });
+        
+// permutations au milieu:
+// s'arranger pour montrer le même nombre de cases de chacune des catégories suivantes
+// (la chute apparaitra donc 5 fois plus que n'importe laquelle des autres cases)
+// tension: 20 17 12 15 (22)
+// détente: 21 16 19 14 (11)
+// chute: 18
+        [20, 15, 18, 17, 12, 18,
+         16, 19, 18, 21, 14, 18,
+         22, 11].forEach(function(n){
+        //[20, 17, 21, 16, 18,   12, 15, 18, 19, 14, 22, 11].forEach(function(n){
+            partition.push({m:144, n:35 + n, t:nextT});
+            nextT += duration / 7;
+            partition.push({m:128, n:35 + n, t:nextT});
+         });
+        
+        notes.slice().reverse().forEach(function(n){
+            partition.push({m:144, n:35 + n, t:nextT});
+            nextT += duration;
+            partition.push({m:128, n:35 + n, t:nextT});
+        });
+        
+        return partition;
+    };
+composition.partition = makePartition();
+
--- imgpiano.js
+++ imgpiano.js
@@ -23,36 +23,76 @@ var getUrlParams = function(href){
             document.head.appendChild(s);
         });
     },
-    run = function(imgDir, imageSequence){
-        var midiSocket = new WebSocket("ws://localhost:8080"),
-            imgParent = document.getElementById('img-parent');
+    buildImages = function(imageSequence, imgDir){
+        var imgParent = document.getElementById('img-parent');
         imageSequence.forEach(function(imgName){
             var h = imgName ? '<img src="'+imgDir+'/'+imgName+'"/>': '<img/>';
             imgParent.innerHTML +=h;
         });
-        var imgs= imgParent.querySelectorAll('img');
-        // for(var i = 0; i < imgs.length; i++){
-        //     imgs[i].style.display = 'none';
-        // }
+        return imgParent.querySelectorAll('img');
+    },
+    showImgForNote = function(imgs, imageSequence, message, note){
+        var noteOn = message === 144 || message === 159,
+            noteOff = message === 128 || message === 143,
+            imgNb = (note - 36) % imageSequence.length ;
+        if(noteOn || noteOff){
+            imgs[imgNb].style.display = noteOn ? 'block':'none';
+        }
+    },
+    synth,
+    play = function(composition, imgs){
+        var v = 100,// velocity
+            start = Date.now(),
+            partition = composition.partition.slice(),
+            doPlay = function(){
+                var time = (Date.now() - start) / 1000;
+                while(partition.length > 0){
+                    var mid = partition[0];
+                    if(mid.t < time){
+                        partition.shift();
+                        synth.send([mid.m, mid.n, v]);
+                        showImgForNote(imgs, composition.imageSequence,
+                                       mid.m, mid.n);
+                    }else{
+                        break;
+                    }
+                }
+                if(partition.length > 0){
+                    requestAnimationFrame(doPlay);
+                }
+            };
+        doPlay();
+    },
+    connectWebSocket = function(imgs, imgDir, composition){
+        var midiSocket = new WebSocket("ws://localhost:8080");
         midiSocket.onmessage = function (event) {
-            var message = JSON.parse(event.data),
-                midiCode = message[1][0],
-                noteOn =  midiCode === 144 || midiCode === 159,
-                noteOff =  midiCode === 128 || midiCode === 143,
-                key = message[1][1],
-                imgNb = (key - 36) % imageSequence.length ;
-            if(noteOn || noteOff){
-                imgs[imgNb].style.display = noteOn ? 'block':'none';
-            }
+            var msgArray = JSON.parse(event.data)[1],
+                message = msgArray[0],
+                note = msgArray[1];
+            showImgForNote(imgs, composition.imageSequence, message, note);
         };
+    },
+    run = function(imgDir, doPlay,composition){
+        var imgs= buildImages(composition.imageSequence, imgDir);
+        connectWebSocket(imgs, imgDir, composition);
+        
+        if(doPlay){
+            play(composition, imgs);
+        }
+        
     };
 
 document.addEventListener('DOMContentLoaded', function(){
     try{
         
-        var imgDir = getUrlParam(location.href,'img') || 'img';
+        var imgDir = getUrlParam(location.href,'img') || 'img',
+            doPlay = getUrlParam(location.href,'play');
+        synth = new WebAudioTinySynth();
+//        synth.send([0xc0,14]); // tubular bells
         loadJs(imgDir+'/img.js').then(function(){
-            run(imgDir, imageSequence);
+            run(imgDir, doPlay, composition);
+        }).catch(function(e){
+            console.error(e);
         });
     }catch(e){ console.error(e); }
 });
