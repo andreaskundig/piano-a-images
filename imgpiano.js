b1031ebbd005629253a8a844e591ab0018f1575f
andreas kundig <andreas.kuendig@gmx.ch>
2017-07-17 23:51:26 +0200

    sprites instead of img to avoid delay between images

--- imgpiano.js
+++ imgpiano.js
@@ -23,36 +23,55 @@ var getUrlParams = function(href){
             document.head.appendChild(s);
         });
     },
-    buildImages = function(imageSequence, imgDir){
-        var imgParent = document.getElementById('img-parent');
-        imageSequence.forEach(function(imgName){
-            var h = imgName ? '<img src="'+imgDir+'/'+imgName+'"/>': '<img/>';
-            imgParent.innerHTML +=h;
-        });
-        return imgParent.querySelectorAll('img');
+    buildSprite = function(imgDir, imgName, width, height, scale){
+        if(!imgName){
+            return null;
+        }
+        // https://stackoverflow.com/questions/9486961/animated-image-with-javascript#9487083
+        var spriteParent = document.getElementById('sprite-parent'),
+            sprite = document.createElement('div'),
+            bgPosX =  Math.round((width - width * scale) / 2);
+        sprite.style.backgroundImage = 'url("'+imgDir+'/'+imgName+'")';
+        sprite.style.backgroundRepeat = 'no-repeat';
+        sprite.style.backgroundSize = (scale * 100) + '%';
+        sprite.style.width = width,
+        sprite.style.backgroundPositionX = bgPosX + 'px',
+        sprite.style.height = Math.floor((height-1) * scale) - 1;
+        spriteParent.appendChild(sprite);
+        return sprite;
     },
-    showImgForNote = function(imgs, imageSequence, message, note){
+    buildSprites = function(imageSequence, imgDir, width, height, scale){
+        return imageSequence.map(function(imgName,i){
+            return buildSprite(imgDir, imgName, width, height, scale);});
+    },
+    showSpriteForNote = function(sprites, imageSequence, message, note,
+                                 height) {
         var noteOn = message === 144 || message === 159,
             noteOff = message === 128 || message === 143,
             imgNb = (note - 36) % imageSequence.length ;
-        if(noteOn || noteOff){
-            imgs[imgNb].style.display = noteOn ? 'block':'none';
+        if(sprites[imgNb] && (noteOn || noteOff)){
+            var offset = noteOn ? height : 0;
+            sprites[imgNb].style.backgroundPositionY = -offset+'px';
         }
     },
     synth,
-    play = function(composition, imgs){
+    play = function(composition, imgs, scale){
         var v = 100,// velocity
             start = Date.now(),
-            partition = composition.partition.slice(),
+            compare = function(a,b){ return a.t - b.t;},
+            partition = composition.partition.slice().sort(compare),
+            height = composition.imageSize.height * scale,
             doPlay = function(){
                 var time = (Date.now() - start) / 1000;
                 while(partition.length > 0){
-                    var mid = partition[0];
-                    if(mid.t < time){
+                    var mid = partition[0],
+                        diff = mid.t - time;
+                    if(diff < 0){
                         partition.shift();
+                        console.log(time, mid.t, diff);
                         synth.send([mid.m, mid.n, v]);
-                        showImgForNote(imgs, composition.imageSequence,
-                                       mid.m, mid.n);
+                        showSpriteForNote(imgs, composition.imageSequence,
+                                       mid.m, mid.n, height);
                     }else{
                         break;
                     }
@@ -61,23 +80,28 @@ var getUrlParams = function(href){
                     requestAnimationFrame(doPlay);
                 }
             };
-        doPlay();
+        // doPlay();
+        setTimeout(doPlay, 2000);
     },
     connectWebSocket = function(imgs, imgDir, composition){
-        var midiSocket = new WebSocket("ws://localhost:8080");
+        var midiSocket = new WebSocket("ws://localhost:8080"),
+            height = composition.imageSize.height / 2;
         midiSocket.onmessage = function (event) {
             var msgArray = JSON.parse(event.data)[1],
                 message = msgArray[0],
                 note = msgArray[1];
-            showImgForNote(imgs, composition.imageSequence, message, note);
+            showSpriteForNote(imgs, composition.imageSequence, message, note,
+                              height);
         };
     },
     run = function(imgDir, doPlay,composition){
-        var imgs= buildImages(composition.imageSequence, imgDir);
-        connectWebSocket(imgs, imgDir, composition);
-        
+        var s = composition.imageSize,
+            scale = Math.min(window.innerWidth/s.width,
+                             window.innerHeight/s.height),
+            sprites = buildSprites(composition.imageSequence, imgDir, s.width, s.height, scale);
+        connectWebSocket(sprites, imgDir, composition, scale);
         if(doPlay){
-            play(composition, imgs);
+            play(composition, sprites, scale);
         }
         
     };
